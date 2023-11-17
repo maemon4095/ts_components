f6cf87119c0dbc1c91fd0ea78b4397735df1d8f6
maemon4095 <maemondecocted@gmail.com>
2023-11-17 13:27:54 +0900

    eq

---
+++ eq.ts
@@ -0,0 +1,83 @@
+export function equals(left: unknown, right: unknown): boolean {
+    if (typeof left !== typeof right) {
+        return false;
+    }
+
+    if (typeof left !== "object") {
+        return left === right;
+    }
+
+    assert<NonNullOrUndefined | null>(right);
+
+    if (left === null) {
+        return right === null;
+    }
+    if (right === null) {
+        return false;
+    }
+
+    if (left.constructor !== right.constructor) {
+        return false;
+    }
+
+    if (left.constructor === Object || left.constructor === Array) {
+        // check right is left
+        for (const name of Object.getOwnPropertyNames(left) as (keyof typeof left)[]) {
+            if (!equals(left[name], right[name])) {
+                return false;
+            }
+        }
+
+        for (const name of Object.getOwnPropertySymbols(left) as (keyof typeof left)[]) {
+            if (!equals(left[name], right[name])) {
+                return false;
+            }
+        }
+
+        // check left is right
+        for (const name of Object.getOwnPropertyNames(right) as (keyof typeof right)[]) {
+            if (!equals(left[name], right[name])) {
+                return false;
+            }
+        }
+
+        for (const name of Object.getOwnPropertySymbols(right) as (keyof typeof right)[]) {
+            if (!equals(left[name], right[name])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    return left === right;
+}
+
+// deno-lint-ignore ban-types
+type NonNullOrUndefined = {};
+function assert<T>(_v: unknown): asserts _v is T { }
+
+
+
+const obj = { a: 1 };
+
+console.log(obj.constructor === Object);
+
+class A {
+
+}
+
+const cls = new A();
+
+console.log(cls.constructor);
+
+const arr: number[] = [];
+console.log(arr.constructor);
+
+console.log(typeof arr);
+
+console.log(typeof cls);
+
+console.log(typeof undefined);
+
+console.log(typeof null);
