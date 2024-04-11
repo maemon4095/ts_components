b275d184894bd94ffcca546046e6556533460eca
maemon4095 <maemondecocted@gmail.com>
2024-04-11 20:31:50 +0900

    add ArrayQueue

---
+++ collections/ArrayQueue.ts
@@ -0,0 +1,55 @@
+export class ArrayQueue<T> {
+    #buffer: T[];
+    #head: number;
+    #count: number;
+
+    constructor(initialCapacity?: number) {
+        this.#buffer = new Array(initialCapacity);
+        this.#count = 0;
+        this.#head = 0;
+    }
+
+    get count() {
+        return this.#count;
+    }
+
+    get capacity() {
+        return this.#buffer.length;
+    }
+
+    get isEmpty() {
+        return this.#count === 0;
+    }
+
+    #resize() {
+        const newbuffer = new Array(Math.max(this.capacity * 2, 16));
+        const count = this.#count;
+        let idx = 0;
+        while (!this.isEmpty) {
+            const item = this.dequeue();
+            newbuffer[idx] = item;
+            idx += 1;
+        }
+        this.#head = 0;
+        this.#count = count;
+        this.#buffer = newbuffer;
+    }
+
+    enqueue(item: T) {
+        if (this.#count == this.capacity) {
+            this.#resize();
+        }
+        this.#buffer[(this.#head + this.#count) % this.capacity] = item;
+        this.#count += 1;
+    }
+
+    dequeue(): T | undefined {
+        if (this.#count === 0) {
+            return undefined;
+        }
+        const head = this.#buffer[this.#head];
+        this.#head = (this.#head + 1) % this.capacity;
+        this.#count -= 1;
+        return head;
+    }
+}
--- collections/mod.ts
+++ collections/mod.ts
@@ -1,1 +1,2 @@
 export { ListQueue } from "./ListQueue.ts";
+export { ArrayQueue } from "./ArrayQueue.ts";
