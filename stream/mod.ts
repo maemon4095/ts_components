13aab2a093d75fd6bf28f2bf39add67feac0c85a
maemon4095 <maemondecocted@gmail.com>
2024-04-12 16:22:18 +0900

    add debounce, transform

--- stream/mod.ts
+++ stream/mod.ts
@@ -1,3 +1,5 @@
+import { bounded } from "../channel/mod.ts";
+
 export function iterableStream<T>(iterable: AsyncIterable<T>): ReadableStream<T> {
     let iter: AsyncIterator<T> | undefined;
     return new ReadableStream({
@@ -20,6 +22,15 @@ export function iterableStream<T>(iterable: AsyncIterable<T>): ReadableStream<T>
     });
 }
 
+export function transform<T, U>(f: (item: T) => U): TransformStream<T, U> {
+    return new TransformStream<T, U>({
+        start() { },
+        transform(chunk, controller) {
+            controller.enqueue(f(chunk));
+        }
+    });
+}
+
 export function asyncIterable<T>(iterable: Iterable<Promise<T>>): AsyncIterable<T> {
     return {
         [Symbol.asyncIterator]() {
@@ -31,6 +42,43 @@ export function asyncIterable<T>(iterable: Iterable<Promise<T>>): AsyncIterable<
                     return { done, value: await value };
                 }
             } satisfies AsyncIterator<T>;
+        }
+    };
+}
+
+export function debounce<T>(ms: number, iterable: AsyncIterable<T>): AsyncIterable<T> {
+    return {
+        [Symbol.asyncIterator](): AsyncIterator<T> {
+            const [sender, receiver] = bounded<IteratorResult<T>>(0);
+            const iter = iterable[Symbol.asyncIterator]();
+
+            let timeout: undefined | number = undefined;
+            new Promise<void>(resolve => {
+                (async () => {
+                    while (true) {
+                        const result = await iter.next();
+                        clearTimeout(timeout);
+                        timeout = setTimeout(() => {
+                            sender.send(result);
+                        }, ms);
+                        if (result.done) break;
+                    }
+
+                    resolve();
+                })();
+            });
+
+            let done = false;
+            return {
+                async next() {
+                    if (done) return { done, value: undefined };
+                    const result = await receiver.receive();
+                    if (result.done) {
+                        done = true;
+                    }
+                    return result;
+                }
+            };
         }
     };
 }
