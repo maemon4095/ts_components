70ae60e725b2061a831cf06c16ef82623fb5b91e
maemon4095 <maemondecocted@gmail.com>
2024-04-18 10:07:51 +0900

    add Ok, Err

--- monadic/mod.ts
+++ monadic/mod.ts
@@ -1,3 +1,15 @@
 export type Ok<T> = { isOk: true, value: T; };
 export type Err<E> = { isOk: false, err: E; };
 export type Result<T, E> = Ok<T> | Err<E>;
+
+export function Ok(): Ok<void>;
+export function Ok<T>(value: T): Ok<T>;
+export function Ok<T>(value?: T): Ok<T> {
+    return { isOk: true, value: value! };
+}
+
+export function Err(): Err<void>;
+export function Err<E>(err: E): Err<E>;
+export function Err<E>(err?: E): Err<E> {
+    return { isOk: false, err: err! };
+}
