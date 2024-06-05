cad045f5955e41eea8f161be94dc65fee6127a8d
maemon4095 <maemondecocted@gmail.com>
2024-06-05 16:21:43 +0900

    add export

--- packages/collections/mod.ts
+++ packages/collections/mod.ts
@@ -1,5 +1,5 @@
 export { ListQueue } from "./ListQueue/mod.ts";
 export { ArrayQueue } from "./ArrayQueue/mod.ts";
-export { Treap } from "./Treap/mod.ts";
+export { Treap, type Comparator } from "./Treap/mod.ts";
 export { PriorityQueue } from "./PriorityQueue/mod.ts";
-export { Arena } from "./Arena/mod.ts";
+export { Arena, type ArenaId } from "./Arena/mod.ts";
